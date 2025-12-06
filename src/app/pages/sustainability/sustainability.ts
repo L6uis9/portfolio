77fcae4f110dd85e4ad9121f6ef7b913cd6e15bd
L6uis9 <louisdavid.duman@gmail.com>
2025-12-06 20:45:16 +0100

    sustainability done

--- src/app/pages/sustainability/sustainability.ts
+++ src/app/pages/sustainability/sustainability.ts
@@ -1,8 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MediaCard } from '../../components/mediacard/mediacard';
 
 @Component({
   selector: 'app-sustainability',
-  imports: [],
+  imports: [CommonModule, MediaCard],
   templateUrl: './sustainability.html',
   styleUrl: './sustainability.scss',
 })

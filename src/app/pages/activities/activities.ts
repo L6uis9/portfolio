1666de1685c6aa3172c86aaa1697c343a2356b4b
L6uis9 <louisdavid.duman@gmail.com>
2025-12-07 17:30:49 +0100

    fix photos

--- src/app/pages/activities/activities.ts
+++ src/app/pages/activities/activities.ts
@@ -10,14 +10,14 @@ import { CommonModule } from '@angular/common';
 })
 export class Activities {
   activities = [
-    { title: 'Hiking', img: '/assets/pictures/randonnee.jpg', alt: 'Hiking' },
-    { title: 'Cycling', img: '/assets/pictures/velo.jpeg', alt: 'Cycling' },
-    { title: 'Cooking', img: '/assets/pictures/oyakodon.jpg', alt: 'Cooking' },
-    { title: 'Board Games', img: '/assets/pictures/chess.jpg', alt: 'Board Games' },
-    { title: 'Puzzles & Strategy games', img: '/assets/pictures/puzzle.jpg', alt: 'Puzzles' },
-    { title: 'Travelling', img: '/assets/pictures/voyage.jpg', alt: 'Travelling' },
-    { title: 'Reading', img: '/assets/pictures/reading.jpeg', alt: 'Reading' },
-    { title: 'Japanese Culture', img: '/assets/pictures/japan.jpg', alt: 'Japanese Culture' },
-    { title: 'Videos-games', img: '/assets/pictures/videogames.jpg', alt: 'Videos-games' }
+    { title: 'Hiking', img: 'assets/pictures/randonnee.jpg', alt: 'Hiking' },
+    { title: 'Cycling', img: 'assets/pictures/velo.jpeg', alt: 'Cycling' },
+    { title: 'Cooking', img: 'assets/pictures/oyakodon.jpg', alt: 'Cooking' },
+    { title: 'Board Games', img: 'assets/pictures/chess.jpg', alt: 'Board Games' },
+    { title: 'Puzzles & Strategy games', img: 'assets/pictures/puzzle.jpg', alt: 'Puzzles' },
+    { title: 'Travelling', img: 'assets/pictures/voyage.jpg', alt: 'Travelling' },
+    { title: 'Reading', img: 'assets/pictures/reading.jpeg', alt: 'Reading' },
+    { title: 'Japanese Culture', img: 'assets/pictures/japan.jpg', alt: 'Japanese Culture' },
+    { title: 'Videos-games', img: 'assets/pictures/videogames.jpg', alt: 'Videos-games' }
   ];
 }
